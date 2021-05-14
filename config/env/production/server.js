0815453daea0eefbb4f24a5bd8f5110ed76d859c
Ioannis-Hz <ioannischtz@gmail.com>
2021-05-14 16:47:04 +0300

    Changes to my-project noted

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
-    admin: {
-        url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-        serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
-      },
+    // admin: {
+    //     url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+    //     serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
+    //   },
   });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://strapi-backend-api-split.herokuapp.com/',
   admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '5510bbd3847a12b343795a27f4748a70'),
-    },
     url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
     serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
   },
