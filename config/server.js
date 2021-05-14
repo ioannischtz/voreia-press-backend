c59cccedf427427e883a8e6ebe7f670aa08fc939
Ioannis-Hz <ioannischtz@gmail.com>
2021-05-14 15:48:20 +0300

    Update database and server configs

--- config/server.js
+++ config/server.js
@@ -5,5 +5,7 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5510bbd3847a12b343795a27f4748a70'),
     },
+    url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
   },
 });
