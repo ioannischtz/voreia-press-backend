ef49ef6cefc98105bb50723cc44ad37da646188f
Ioannis-Hz Development <johnyvoyager@gmail.com>
2021-05-16 20:23:41 +0300

    rebuilt and moved admin panel on heroku too

--- config/server.js
+++ config/server.js
@@ -1,3 +1,5 @@
+const forgotPasswordTemplate = require('./email-templates/forgot-password');
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -6,7 +8,12 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5510bbd3847a12b343795a27f4748a70'),
     },
-    url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
+    url: '/dashboard', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+    serveAdminPanel: true, // http://yourbackend.com will not serve any static admin files
+    forgotPassword: {
+      from: 'voreia.press@gmail.com',
+      replyTo: 'voreia.press@gmail.com',
+      emailTemplate: forgotPasswordTemplate,
+    },
   },
 });
