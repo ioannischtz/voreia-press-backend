20d7a3af389e9b1594cdbb5e63e1095415c30748
ioannischtz <51233399+ioannischtz@users.noreply.github.com>
2023-05-31 16:20:19 +0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5510bbd3847a12b343795a27f4748a70'),
     },
-    url: 'https://voreia-press-admin.herokuapp.com/dashboard', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+    url: 'https://voreia-press-admin.onrender.com/dashboard', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
     serveAdminPanel: true, // http://yourbackend.com will not serve any static admin files
     forgotPassword: {
       from: 'voreia.press@gmail.com',
